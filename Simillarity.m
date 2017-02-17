3d01227a7f6ebe1d52ab1e0929b80631f369e3da
Moein Khajehnejad <moeen7104@gmail.com>
2017-02-18 02:33:18 +0330

    Update Simillarity.m

--- Simillarity.m
+++ Simillarity.m
@@ -2,8 +2,7 @@ function S = Simillarity(Alpha,P,A)
     N=length(A);
     S=inv(eye(N)-Alpha.*P);
     %If Alpha is not a constant matrix you can refer to our paper and simply replace the equation for 
-    %similarity matrix. You can notice that the summation does not need to go on after a specific 
-    %amount of steps according to the network structure
+    %similarity matrix. 
     %[V,D] = eig(P);
     %S=zeros(N,N);
     %for each k=1:N
