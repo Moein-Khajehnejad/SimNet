8906d473f99264d9a3e80ee3e0b1242d999e2edd
Moein Khajehnejad <moeen7104@gmail.com>
2017-02-18 10:57:28 +0330

    Update Simillarity.m

--- Simillarity.m
+++ Simillarity.m
@@ -3,7 +3,7 @@ function S = Simillarity(Alpha,P,A)
     S=inv(eye(N)-Alpha.*P);
     %If Alpha is not a constant matrix you can refer to our paper and simply replace the equation for 
     %similarity matrix. 
-    %[V,D] = eig(P);
+    %[V,D] = eig(A);
     %S=zeros(N,N);
     %for each k=1:N
     % calculate S(i,j)=S(i,j)+(V(i,k)*(V(k,j))^(-1))*(1/(1-Alpha(i,j)*D(k,k)));;
