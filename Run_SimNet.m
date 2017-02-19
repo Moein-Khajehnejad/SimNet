cf55c4a85a12bef3688c5c87de75f0db3f53fdac
Moein Khajehnejad <moeen7104@gmail.com>
2017-02-20 00:31:07 +0330

    Update Run_SimNet.m

--- Run_SimNet.m
+++ Run_SimNet.m
@@ -1,4 +1,5 @@
-function NetRep= Run_SimNet(Adjacency,dimension)
+function NetRep= Run_SimNet(Adjacency,dimension,c)
+%If c is set to 1, a constant Alpha is used, other wise of c=0, Alpha is a dynamic damping factor
 
 %Adjacency Matrix Is Loaded
 P=ScaleMat(Adjacency); %matrix is scaled
@@ -9,16 +10,20 @@ W = Adjacency - diag(diag(Adjacency));  %diagonal elements must be 0
 D = diag(sum(W), 0); %degree matrix
 L_plus=pinv(D-Adjacency); %Moore-Penrose pseudoinverse
 
-%This section is used if you wish to use dynamic damping factors.Please refer to our paper for more information.
-%Calculating Matrix Alpha
+
 Alpha=zeros(N,N);
-%for i=1:N
-    %for j=1:N
-        %Alpha(i,j)=(atan(L_plus(i,j))+pi/2)/pi;
-    %end
-%end
-%As a more simple case a constant matrix can be used as Alpha
+if (c)
 Alpha = 0.5;
+end
+
+else
+for i=1:N
+    for j=1:N
+        Alpha(i,j)=(atan(L_plus(i,j))+pi/2)/pi;
+    end
+end
+
+
 S  = Simillarity(Alpha,Adjacency);
 
 k=1; %constant,no need to use higher steps
